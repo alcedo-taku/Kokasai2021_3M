227cf86f60420b1d62b44effab9b08b9c152ec57
立脇拓 <takuji.coroton.thanks@gmail.com>
2021-09-20 00:47:57 +0900

    feat(rifle): UART 送信用のプログラムを追加

--- Infrared_Rifle/Core/Src/wrapper.cpp
+++ Infrared_Rifle/Core/Src/wrapper.cpp
@@ -1,6 +1,8 @@
 #include "wrapper.hpp"
 
 /* Include Begin */
+#include "gpio.h"
+#include "usart.h"
 /* Include End */
 
 /* Enum Begin */
@@ -10,6 +12,7 @@
 /* Struct End */
 
 /* Variable Begin */
+const uint8_t rifle_id = 0;
 /* Variable End */
 
 /* Class Constructor Begin */
@@ -23,7 +26,11 @@ void init(void){
 }
 
 void loop(void){
-
+	if( HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_9) ){
+		HAL_UART_Transmit(&huart2, (uint8_t *)&rifle_id, sizeof(rifle_id), 500);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
+	}
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
 }
 
 /* Function Body Begin */

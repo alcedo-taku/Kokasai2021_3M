67bd11640aed13c4c2c549c3aae905d4b8270c76
立脇拓 <takuji.coroton.thanks@gmail.com>
2021-09-21 01:17:39 +0900

    chore(rifle): HCLKを32MHｚにし、LED用PWM周波数を38kHzにした

--- Infrared_Rifle/Core/Src/tim.c
+++ Infrared_Rifle/Core/Src/tim.c
@@ -92,7 +92,7 @@ void MX_TIM21_Init(void)
   htim21.Instance = TIM21;
   htim21.Init.Prescaler = 0;
   htim21.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim21.Init.Period = 65535;
+  htim21.Init.Period = 842;
   htim21.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim21.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_PWM_Init(&htim21) != HAL_OK)

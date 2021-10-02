a5d67871240ff921d2c91793be2e6ea22cfcb45a
立脇拓 <takuji.coroton.thanks@gmail.com>
2021-10-02 22:40:58 +0900

    feat(main): 的の数を可変にした

--- Central_Cotrol_Circuit/Core/Src/wrapper.cpp
+++ Central_Cotrol_Circuit/Core/Src/wrapper.cpp
@@ -10,6 +10,8 @@
 #include <bitset>
 /* Include End */
 
+constexpr uint8_t NUMBER_OF_TARGET = 15;
+
 /* Enum Begin */
 /* Enum End */
 
@@ -19,7 +21,7 @@ struct GPIO{
 	uint16_t GPIO_Pin;
 };
 GPIO uart_test = {GPIOB, GPIO_PIN_3};
-constexpr std::array<GPIO,15> uart_pin = {
+constexpr std::array<GPIO,NUMBER_OF_TARGET> uart_pin = {
 		GPIO{GPIOA, GPIO_PIN_3},
 		GPIO{GPIOA, GPIO_PIN_2},
 		GPIO{GPIOA, GPIO_PIN_10},
@@ -45,18 +47,15 @@ constexpr std::array<GPIO,2> led_pin = {
 /* Struct End */
 
 /* Variable Begin */
-constexpr uint8_t NUMBER_OF_TARGET = 15;
 std::array<uint8_t,4> score;
-constexpr std::array<uint8_t,NUMBER_OF_TARGET> SCORE_OF_TARGET = {1,1,1,1,1,1,1,1,1,1,1};
+constexpr std::array<uint8_t,NUMBER_OF_TARGET> SCORE_OF_TARGET = {1,1,1,1,1,1,1,1,1,1,1}; // 各的の点数
 uint8_t target_debug;
-uint8_t uart_success;
-std::array<uint8_t,NUMBER_OF_TARGET> led_blinking_count;
+std::array<uint8_t,NUMBER_OF_TARGET> led_blinking_count; // LEDの点滅回数をカウント
 /* Variable End */
 
 /* Class Constructor Begin */
 DFRobotDFPlayerMini DFPlayerMini;
 std::array<UART_by_GPIO,NUMBER_OF_TARGET> uart_by_gpio = {UART_by_GPIO(), UART_by_GPIO()};
-//UART_by_GPIO uart_by_gpio(uart_by_gpio[0].get_data(), GPIOB, GPIO_PIN_3);
 /* Class Constructor End */
 
 /* Function Prototype Begin */
@@ -80,22 +79,26 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
     if(htim == &htim17){ // 1200Hz
     	static std::array<uint16_t,NUMBER_OF_TARGET> received_success_count = {0};
 
-    	received_success_count[0]--;
-    	if(received_success_count[0] == 0)
-    		received_success_count[0] = 1;
+    	for(uint8_t i=0; i<NUMBER_OF_TARGET; i++){ // 的の数だけループ
+        	received_success_count[i]--; // 毎回、受信成功カウントを少しずつ減らす
+        	if(received_success_count[i] == 0) // アンダーフロー防止処理
+        		received_success_count[i] = 1;
 
-    	uart_by_gpio[0].call_with_timer_interrupt();
-    	if(uart_by_gpio[0].is_successful_reception()){
-			target_debug = uart_by_gpio[0].get_data();
-			if(0 <= uart_by_gpio[0].get_data() && uart_by_gpio[0].get_data() <=3){ // 受信値が銃のIDだったら
-				received_success_count += 10*100; // 受信成功カウントを上げる
-			}
-    	}
-    	if(10*1000 < received_success_count){ // 受信成功カウントが一定値を超えたら
-			score[uart_by_gpio[0].get_data()] += SCORE_OF_TARGET[0]; // その時のIDに得点を入れる
-			led_blinking_count[0] = 0;
+        	uart_by_gpio[i].call_with_timer_interrupt();
+        	if(uart_by_gpio[i].is_successful_reception()){
+    			target_debug = uart_by_gpio[i].get_data();
+    			if(0 <= uart_by_gpio[i].get_data() && uart_by_gpio[i].get_data() <=3){ // 受信値が銃のIDだったら
+    				received_success_count[i] += 10*100; // 受信成功カウントを増やす
+    			}
+        	}
+
+        	if(10*1000 < received_success_count[i]){ // 受信成功カウントが一定値を超えたら
+    			score[uart_by_gpio[i].get_data()] += SCORE_OF_TARGET[i]; // その時のIDに得点を入れる
+    			led_blinking_count[i] = 0;
+        	}
     	}
 
+    	// Debug用
     	if(uart_by_gpio[0].get_data() == 3){
     		HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
     	}else{
@@ -103,9 +106,12 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
     	}
     }
     else if(htim == &htim16){ // 4Hz
-    	if(led_blinking_count[0] < 4){
-    		HAL_GPIO_TogglePin(led_pin[0].GPIOx, led_pin[0].GPIO_Pin);
-    		led_blinking_count[0]++;
+    	// 的に当たった時、LEDを点滅させる
+    	for(uint8_t i=0; i<NUMBER_OF_TARGET; i++){ // 的の数だけループ
+        	if(led_blinking_count[i] < 4){
+        		HAL_GPIO_TogglePin(led_pin[i].GPIOx, led_pin[i].GPIO_Pin);
+        		led_blinking_count[i]++;
+        	}
     	}
     }
 }
